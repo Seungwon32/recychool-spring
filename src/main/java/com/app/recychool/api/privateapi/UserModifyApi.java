65d912f3a08e556bc37394776be0df846f7a3e36
Seungwon32 <bsw030722@gmail.com>
2025-12-22 16:07:44 +0900

    1222E

--- src/main/java/com/app/recychool/api/privateapi/UserModifyApi.java
+++ src/main/java/com/app/recychool/api/privateapi/UserModifyApi.java
@@ -1,6 +1,9 @@
 package com.app.recychool.api.privateapi;
 
+import com.app.recychool.domain.dto.UserPasswordUpdateDTO;
+import com.app.recychool.domain.dto.UserResponseDTO;
 import com.app.recychool.domain.dto.UserUpdateDTO;
+import com.app.recychool.service.UserModifyImpl;
 import com.app.recychool.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 
 public class UserModifyApi {
+    private final UserModifyImpl userModifyImpl;
     private final UserService userService;
 
     @PatchMapping("/{id}")
@@ -19,7 +23,23 @@ public class UserModifyApi {
             @PathVariable("id") Long userId,
             @RequestBody UserUpdateDTO userUpdateDTO) {
 
-        userService.updateUserInfo(userId, userUpdateDTO);
+        userModifyImpl.updateUserInfo(userId, userUpdateDTO);
         return ResponseEntity.ok("회원 정보가 성공적으로 수정되었습니다.");
     }
+
+    @PatchMapping("/{id}/password")
+    public ResponseEntity<String> updatePassword(
+            @PathVariable Long id,
+            @RequestBody UserPasswordUpdateDTO dto) {
+
+        userModifyImpl.updatePassword(id, dto);
+        return ResponseEntity.ok("비밀번호가 변경되었습니다.");
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
 }
